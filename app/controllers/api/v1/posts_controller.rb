e077a01a8025fe5ff6cc2ac32862b932ae894a6c
yani82 <yani.iman1@gmail.com>
2021-03-17 00:20:25 -0400

    post controller index and create routes

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,25 @@
 class Api::V1::PostsController < ApplicationController
+    # name space isn't make or break 
 
+    def index 
+        posts = Post.all 
+        render json: posts 
+        # do not need instance variable because of fetch request 
+    end 
+
+    def create 
+        post = Post.new(post_params)
+        if post.save
+            render json: post, status: :accepted
+        else 
+            render json: { errors: post.errors.full_messages }, status: :unprocessible_entity 
+        end 
+    end 
+
+    private 
+
+    def post_params
+        params.require(:post).permit(:title, :content, :image_url, :user_id)
+    end 
 
 end

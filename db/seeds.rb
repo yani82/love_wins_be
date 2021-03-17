29a7e86d3220bf8d9bb0680c2e1d30492307ffd5
yani82 <yani.iman1@gmail.com>
2021-03-17 01:49:09 -0400

    implemented post and user serializers

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 # User.destroy_all 
 # Post.destroy_all 
 
-Mariah = User.create(name: "Mariah")
-Whitney = User.create(name: "Whitney") 
-Aaliyah = User.create(name: "Aaliyah") 
+Mariah = User.create(name: "Mariah", image_url: "https://pbs.twimg.com/profile_images/1346569733454376971/LUBieasg.jpg", email: "mariah@example.com")
+Whitney = User.create(name: "Whitney", image_url: "https://basehologram.com/transforms/09147396c4e2da9ef23b64d8407e78e8/3533/555200023_523ddf5360fbd4be7509b9c39e190837.jpg", email: "whitney@example.com") 
+Aaliyah = User.create(name: "Aaliyah", image_url: "https://i.pinimg.com/originals/e2/d9/ae/e2d9ae57b885482268013fcf2a0fb7b0.jpg", email: "aaliyah@example.com") 
 
 Post.create(title: "The Food Bank for New York City", content: "No one should be hungry", image_url: "https://www.foodbanknyc.org/wp-content/uploads/mission_moment.jpg", user_id: Mariah.id)
 

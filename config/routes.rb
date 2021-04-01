a86be802b70b413e39044887c17fae0a3edba9b9
yani82 <yani.iman1@gmail.com>
2021-04-01 15:28:21 -0400

    still debugging delete function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
 namespace :api do  
   namespace :v1 do
-    resources :posts, only: [:index, :create]
+    resources :posts
     resources :users, only: [:index]
     end 
 end 
